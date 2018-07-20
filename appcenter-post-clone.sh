5c81f0cbab1dd677d5cefb717b80eae0d00e640c
Jihye Eom <jeom327@outlook.com>
2018-07-20 19:03:52 +0900

    added

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,10 +1,2 @@
 #!/usr/bin/env bash
-#
-# Report build status next to github commit.
-#
-# Adjust settings in github.sh file
-#
-# Contributed by: Mirosław Zawisza
-# https://zeyomir.github.io
-
 export NODE_BINARY='node --expose-gc --max_old_size=8192'
